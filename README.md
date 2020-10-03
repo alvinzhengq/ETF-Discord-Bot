b9da40f3ecccf2952a5b3958036951903b995ef6
alvinzhengq <vortexdomainia@gmail.com>
2020-10-02 17:14:14 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ETF-Discord-Bot
+
+A Discord Bot for cross checking member validity and grating access to the ETF server accordingly.
---
+++ bot.js
@@ -0,0 +1,81 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { http, https } = require('follow-redirects');
+const fs = require('fs');
+const csv = require('fast-csv');
+const configs = JSON.parse(require('fs').readFileSync("./config.json", "utf-8"))
+
+client.on("ready", () => {
+
+    let etfGuild = client.guilds.cache.get(configs.etf_id)
+
+    client.on("message", (msg) => {
+        if(msg.author.bot == false && msg.channel.type == 'dm'){
+            let name = msg.content.toUpperCase();
+
+            isVerified(name, (result) => {
+                if(result == "ERROR"){
+                    msg.channel.send("Something went wrong, please message the bot creator: Cox#5431 for help. Error code: CRANKWORKX")
+
+                }else if(result == true){
+                    etfGuild.member(msg.author).setNickname(getProperName(name))
+                    .then(() => {
+                        etfGuild.member(msg.author).roles.add(etfGuild.roles.cache.find(role => role.name == "Member"))
+                        .then(()=>{
+                            msg.channel.send("You have been given access to the ETF server, make sure to read over all the rules!")
+                        })
+                        .catch(()=>{
+                            msg.channel.send("Something went wrong, please message the bot creator: Cox#5431 for help. Error code: MILLERS")
+                        })
+                    })
+                    .catch((err)=>{
+                        console.log(err)
+                        msg.channel.send("Something went wrong, please message the bot creator: Cox#5431 for help. Error code: KEVIN")
+                    })
+
+                }else if(result == false){
+                    msg.channel.send("Name not found, please check if you spelled your name correctly(first and last), or fill out the signup form(http://bit.ly/ETFWebDev) if you haven't already.");
+
+                }
+            })
+        }
+    });
+
+});
+
+function getProperName(name){
+    return name
+    .toLowerCase()
+    .split(' ')
+    .map(function(word) {
+        return word[0].toUpperCase() + word.substr(1).toLowerCase();
+    })
+    .join(' ');
+}
+
+function isVerified(name, cb){
+    downloadFile(()=>{
+        var readStream = require('fs').createReadStream("./members.csv")
+        .pipe(csv.parse({ headers: true }))
+        .on('error', error => cb("ERROR"))
+        .on('data', row => {
+            if(row["Student Name (first last)"].toUpperCase() == name){
+                cb(true);
+                readStream.destroy();
+            }
+        })
+        .on('end', rowCount => cb(false));
+    })
+}
+
+function downloadFile(cb){
+    let file = fs.createWriteStream("members.csv");
+    https.get(configs.sheet_url, function(response) {
+        response.pipe(file);
+    });
+    file.on("close", ()=>{
+        cb();
+    })
+}
+
+client.login(configs.token);
